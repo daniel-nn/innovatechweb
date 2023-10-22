236e0a26d94bb0a897a64a2f70e51a40bd6d1e85
Brayan Alexis Salazar Rivas <brayansalazar.dev@gmail.com>
2023-10-22 15:21:44 -0500

    10/22/2023

---
+++ js/Contact.js
@@ -0,0 +1,22 @@
+function sendMail(event){
+    event.preventDefault();
+    console.log(event)
+        let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("messaje").value,
+        phone: document.getElementById("phone").value
+    }
+    console.log("Hola")
+    emailjs.send("service_2dwjuz9", "template_yb9kd2r", params)
+    .then((res) => {
+            console.log(res)
+           Swal.fire("You Got It", "We have received your information, our team will contact you as soon as possible.", "success")
+          document.getElementById("name").value=""
+          document.getElementById("email").value=""
+          document.getElementById("email").value=""
+          document.getElementById("messaje").value=""
+        })
+        .catch((err) => console.log(err))
+
+}
